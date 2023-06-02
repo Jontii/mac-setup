f7c9c412cffbb41b4944a6a26b038a6db78f51c1
Jonathan Bodling <jonathan.bodling@mynt.se>
2023-06-02 13:16:23 +0200

    chore: add todos

--- setup.sh
+++ setup.sh
@@ -5,6 +5,8 @@ xcode-select --install
 echo "Installing brew"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+# Need to set brew path 
+
 echo "Updating homebrew..."
 brew update
 
@@ -15,6 +17,8 @@ echo "Install node"
 brew install node
 
  # Install node@16 and yarn and update npm to latest
+
+ #Just add docker desktop ? 
 
 echo "Install docker"
 brew install docker
@@ -29,16 +33,16 @@ echo "Install AWS VPN Client"
 brew install aws-vpn-client
 
 echo "Install slack"
-brew install slack
+brew install --cask slack
 
 echo "Install VS-Code"
-brew install visual-studio-code
+brew install --cask visual-studio-code
 
 echo "Install Spotify"
-brew install spotify
+brew install --cask spotify
 
 echo "Install Chrome"
-brew install google-chrome
+brew install --cask google-chrome
 
 echo "Cleaning up brew"
 brew cleanup
